509f5419a50458bb7588d2cffe9cf8bb73d0503c
manukumarss <manu.vaar@gmail.com>
2018-01-09 13:54:57 +0530

    track label updated

--- app/src/main/java/com/srirangadigital/tungalahari/AlbumsAdapter.java
+++ app/src/main/java/com/srirangadigital/tungalahari/AlbumsAdapter.java
@@ -54,7 +54,7 @@ public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.MyViewHold
     public void onBindViewHolder(final MyViewHolder holder, int position) {
         Album album = albumList.get(position);
         holder.title.setText(album.getTitle());
-        holder.count.setText(album.getNumOfSongs() + " songs");
+        holder.count.setText(album.getNumOfSongs() + " ವಚನಗಳು");
 
         // loading album cover using Glide library
         Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
